fada233c3fbee0031a40695f88b0ea3fbf9b22b2
mbenson025 <mbenson025@gmail.com>
2022-09-22 20:46:45 -0500

    fixes for win/ lose conditions

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Python Hangman
+
+<br>
+
+<img src="assets/hangmanlogo.jpg" alt="logo" title="Hangman Title">
+
+<br>
+
+Command line application to practice Python basics
--- main.py
+++ main.py
@@ -1,29 +1,30 @@
 # python_hangman
 
 import random
-from ascii import title
-from ascii import stages
+from ascii import title, stages
 from words import word_bank
-
 
 end_game = False
 lives = 6
 
-word_bank = ["amusement", "balance", "chemical", "development"]
 round_word = random.choice(word_bank)
 print(title)
-print(f"Round word is {round_word}")
+# uncomment below to test
+# print(f"Round word is {round_word}")
 
 game_display = []
 for each_letter in range(len(round_word)):
     game_display += "_"
-
-already_guessed = []
+print(f"{' '.join(game_display)}")
+print(" ")
 
 while not end_game:
     guess = input("Guess a letter: ").lower()
+
     # compare user guess to round word
     if guess in round_word:
+        if guess in game_display:
+            print(f"You've already tried the letter {guess}!")
         accumulator = 0
         for letter in round_word:
             if letter == guess:
@@ -31,15 +32,15 @@ while not end_game:
                 game_display[accumulator] = guess
                 # add to accumulator at the end of the loop for correct letter position
             accumulator += 1
-        print(game_display)
     if guess not in round_word:
         lives -= 1
-        print(f"Wrong. Remaining lives: {lives}")
+        print(f"Sorry, {guess} is not correct. Remaining lives: {lives}")
         if lives == 0:
-            print("You Lose!")
+            print(f"You Lose! The word was {round_word}")
             end_game = True
     print(stages[lives])
-    print(game_display)
+    print(f"{' '.join(game_display)}")
+    print(" ")
 
 
 if "_" not in game_display:
